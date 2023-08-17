bc56dbf57ad8614721b68a1a57f6454661208a76
Daniel Manosalva <manosalvasierra@gmail.com>
2023-08-16 21:01:33 -0500

    feat: :zap: endpoints de translado
    se termino de trabajar en los endpoints de translado
    
    dannkol

--- review_2/prueba.js
+++ review_2/prueba.js
@@ -1,36 +1,62 @@
-import { MongoClient } from "mongodb";
-
-
-const uri = 'mongodb+srv://Dannkol:diminombre12A@pruebas.ncnxgtj.mongodb.net/'; // Cambia la URI de acuerdo a tu configuración
-const dbName = 'db_bodegas_campus';
-
-(async () => {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-
+static async transladoProductos ({ object, object2 }) {
+  const db = await connect()
+  const session = client.startSession() // inicia una nueva sesion en la bd
   try {
-    await client.connect();
+    session.startTransaction() // inicia una transaccion en la sesion
 
-    const database = client.db("db_bodegas_campus");
-    const bodegasCollection = database.collection("bodegas");
+    // A partir de este punto, todas las operaciones dentro de este bloque formarán parte de la misma transacción.
+    const inventarios = db.collection('inventarios')
+    const historiales = db.collection('historiales')
 
-    const bodegaData = {
-      nombre,
-      id_responsable,
-      estado
-    };
+    const inventarioOrigen = await inventarios.findOneAndUpdate(
+      { id_producto: object.idProducto, id_bodega: object.idBodegaOrigen, cantidad: { $gte: object.cantidad } },
+      { $inc: { cantidad: -object.cantidad } },
+      { session, returnOriginal: false }
+    )
 
-    const result = await bodegasCollection.insertOne(bodegaData);
-    const bodegaId = result.insertedId;
+    if (inventarioOrigen.value) {
+      const inventarioDestino = await inventarios.findOneAndUpdate(
+        {
+          id_producto: object.idProducto,
+          id_bodega: object.idBodegaDestino
+        },
+        { $inc: { cantidad: object.cantidad } },
+        { session, returnOriginal: false }
+      )
 
-    const bodega = await bodegasCollection.findOne({ _id: bodegaId });
+      if (inventarioDestino.value) {
+        const historial = {
+          id_bodega_origen: object2.idBodegaOrigen,
+          id_bodega_destino: object2.idBodegaDestino,
+          cantidad: object2.cantidad,
+          id_inventario: inventarioOrigen.value._id
+        }
 
-    console.log(result);
-    console.log(bodega.nombre);
+        const result = await historiales.insertOne(historial, { session })
 
-    return bodega;
+        await session.commitTransaction() // Confirma la transacción.
+        session.endSession() // cierra la sesion
+
+        const { insertedId } = result
+
+        return { insertedId, message: 'Transaccion exitosa' }
+      } else {
+        await session.abortTransaction()
+        session.endSession()
+
+        return 'No se pudo actualizar inventario destino'
+      }
+    } else {
+      await session.abortTransaction() // Revoca la transacción en caso de algún error.
+      session.endSession() // Finaliza la sesión en caso de error.
+
+      return 'No se pudo actualizar inventario origen o no hay suficiente stock'
+    }
   } catch (error) {
-    throw error;
+    await session.abortTransaction()
+    session.endSession()
+    return 'Error durante la transacción'
   } finally {
-    client.close();
-  } 
-})();
+    await closeConnection()
+  }
+}
